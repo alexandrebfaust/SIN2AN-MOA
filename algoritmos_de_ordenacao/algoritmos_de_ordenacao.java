367eb6dc6c109f6fe5248b40f656fc51f38b8337
Alexandre B Faust <alexandrebfaust@gmail.com>
2019-09-04 20:30:44 -0300

    Update algoritmos_de_ordenacao.java

--- algoritmos_de_ordenacao/algoritmos_de_ordenacao.java
+++ algoritmos_de_ordenacao/algoritmos_de_ordenacao.java
@@ -1,13 +1,11 @@
-package aula2;
-
 import java.util.Scanner;
 
-public class algoritmos_de_ordenacao {
+public class bubble_sor {
 
 	public static void main(String[] args) {
 		
 		int x = 0; //Tamanho do vetor
-		int y = 0; //Variavel do vetor
+		int y = 0; //Variavel flutuante do vetor
 
 		Scanner entrada = new Scanner(System.in);
 		do {
@@ -23,11 +21,35 @@ public class algoritmos_de_ordenacao {
 			vetor[i] = entrada.nextInt();
 		}
 
+		System.out.print("Vetor desordenado: ");
 		Vetor v = new Vetor();
 		v.imprimir(vetor);
+		System.out.print("\n\n\n");
 
+		for(j=1;j<=n;j++){
+			// 0 1 2 3 4 -> 
+			for(i=0;i<n-1;i++){
+				y = vetor[j];
+				if(vetor[i]>)
 
+			}
 
+		}
+
+		for(i=0; i<x; i++){
+			for (j=0; j<(x-1); j++) {
+				if(vetor[j] > vetor[j+1]){
+					y = vetor[j];
+					vetor[j] = vetor[j+1];
+					vetor[j+1] = y;
+				}
+			}
+
+		}
+
+		System.out.print("Vetor ordenado: ");
+		Vetor v = new Vetor();
+		v.imprimir(vetor);
 
 	}
 
